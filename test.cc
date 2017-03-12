426febdce3980b8e376087cbc6cbd4be59aeca6b
Dylan McGannon <me@dyl.dog>
2017-03-12 16:44:05 +1100

    Modified the test program

--- test.cc
+++ test.cc
@@ -13,38 +13,43 @@ int main (int argc, char **argv)
     srand (((unsigned long long)time.tv_sec * 1000000) + time.tv_usec);
 
     //Generate the standard pattern database (used for 4x4 puzzles.
-    generate_standard_pattern_databases();
+    //generate_standard_pattern_databases();
 
-    //Return for now for debugging.
-    return 0;
+    size_t board_size;
+    size_t solvable_count;
+    std::string solved_puzzle;
+    std::string solvable_puzzle;
+    std::string unsolvable_puzzle;
+    std::string random_puzzle;
 
     /******************************
      *  BOARD SIZE 3
      ******************************/
     std::cout << "BOARD SIZE 3" << std::endl;
-    size_t board_size = 3;
-    size_t solvable_count = 0;
+    board_size = 3;
+    solvable_count = 0;
     for (size_t i=0; i<1000; i++) {
         std::vector<size_t> puzzle = taquin_generate_vector(board_size);
         solvable_count += taquin_check_solvable(puzzle, board_size);
     }
     std::cout << solvable_count << "/1000 generated puzzles solvable" << std::endl;
 
-    std::string solved_puzzle = "1 2 3 4 5 6 7 8 0";
+    solved_puzzle = "1 2 3 4 5 6 7 8 0";
     Board solved(taquin_tokenise_board_string(solved_puzzle), board_size);
     std::cout << "Solved test: " << taquin_check_solvable(solved_puzzle, board_size) << std::endl;
     std::cout << "\tInversions: " << taquin_get_inversion_count(solved.get_state(), board_size) << std::endl;
     std::cout << "\tHeuristic: " << solved.get_heuristic() << std::endl;
+    std::cout << "\tHash: " << std::hex << solved.get_state_hash() << std::dec << std::endl;
     std::cout << "\tCheck Solved: " << solved.check_solved() << std::endl;
 
-    std::string solvable_puzzle = "4 5 7 2 8 0 6 1 3";
-    std::string unsolvable_puzzle = "5 4 7 2 8 0 6 1 3";
+    solvable_puzzle = "4 5 7 2 8 0 6 1 3";
+    unsolvable_puzzle = "5 4 7 2 8 0 6 1 3";
     std::cout << "Solvable test: " << taquin_check_solvable(solvable_puzzle, board_size) << std::endl;
     std::cout << "\tHeuristic: " << Board(taquin_tokenise_board_string(solvable_puzzle), board_size).get_heuristic() << std::endl;
     std::cout << "\tMoves: " << taquin_solve(solvable_puzzle, board_size).size() << std::endl;
     std::cout << "Unsolvable test: " << taquin_check_solvable(unsolvable_puzzle, board_size) << std::endl;
 
-    std::string random_puzzle = taquin_generate_string(board_size);
+    random_puzzle = taquin_generate_string(board_size);
     std::cout << std::endl;
     std::cout << "Test random: " << random_puzzle << std::endl;
     std::cout << "Solvable: " << taquin_check_solvable(random_puzzle, board_size) << std::endl;
@@ -70,6 +75,7 @@ int main (int argc, char **argv)
     std::cout << "Solved test: " << taquin_check_solvable(solved_puzzle, board_size) << std::endl;
     std::cout << "\tInversions: " << taquin_get_inversion_count(solved_2.get_state(), board_size) << std::endl;
     std::cout << "\tHeuristic: " << solved_2.get_heuristic() << std::endl;
+    std::cout << "\tHash: " << std::hex << solved_2.get_state_hash() << std::dec << std::endl;
     std::cout << "\tCheck Solved: " << solved_2.check_solved() << std::endl;
 
     solvable_puzzle = "12 1 10 2 7 11 4 14 5 0 9 15 8 13 6 3";
