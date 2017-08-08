91c6ebd9a6e8ae91620185fdc7b5869360bc8683
Dylan McGannon <peltium@gmail.com>
2017-08-08 19:29:29 +1000

    Add a simple pattern database generation test.

--- test/check-generate-patterndb.cc
+++ test/check-generate-patterndb.cc
@@ -5,13 +5,21 @@
 
 using namespace TaquinSolve;
 
-static void test_generate_pattern_db()
+static void test_generate_standard_pattern_db()
 {
     generate_standard_pattern_databases();
 }
 
+static void test_generate_pattern_db()
+{
+    std::vector<uint8_t> goal_board = taquin_tokenise_board_string("1 2 3 0");
+    std::set<uint8_t> group_tiles = {1,2,3};
+    generate_pattern_database(goal_board, group_tiles, 2, "./123.db.bin");
+}
+
 int main (void)
 {
+    test_generate_standard_pattern_db();
     test_generate_pattern_db();
 
     return EXIT_SUCCESS;
